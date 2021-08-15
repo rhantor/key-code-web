8dff1a20c276c9c501a606707067df6c22b00409
RH Antor <rhantor19@gmail.com>
2021-08-16 01:01:32 +0600

    key code app

---
+++ main.js
@@ -0,0 +1,30 @@
+let key = document.querySelector(".key h2");
+
+let keyCode = document.querySelector(".keyCode h2");
+let keyCodeDiv = document.querySelector('.keyCode');
+let copied = document.querySelector('.keyCode p')
+let overlay = document.querySelector('.overlay')
+window.addEventListener('keydown' ,(e)=>{
+    overlay.classList.add('hide')
+    key.innerText = e.key ;
+    keyCode.innerText = e.keyCode ;
+if(e.keyCode === 32){
+    key.innerText = "space"
+}
+})
+
+keyCodeDiv.addEventListener('click' ,(e)=>{
+    const textarea = document.createElement('textarea');
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = "absolute";
+    textarea.value = keyCodeDiv.querySelector('h2').innerText ;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    copied.innerText = 'Copied!'
+    document.body.removeChild(textarea);
+
+    setTimeout(() => {
+        copied.innerText = 'Click to copy'
+    }, 2000);
+})
